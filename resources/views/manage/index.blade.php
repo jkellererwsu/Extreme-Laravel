a32adba04357d8dbb413f8d86af6738d0b92a4f1
jkellerer <webmaster@extremenazarene.org>
2016-08-05 18:19:09 -0500

    just updated index for manage page

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -4,7 +4,7 @@
     <h1>Users</h1>
     <div class="list-group">
         @foreach ($users as $user)
-            <a href="{{ action('ContactsController@show', [$user->id])}}" class="list-group-item">ID: {{$user->id}} - {{$user->email}} - {{$user->roles()->pluck('name')}}</a>
+            <a class="list-group-item">ID: {{$user->id}} - {{$user->email}} - {{$user->roles()->pluck('name')}}</a>
         @endforeach
     </div>
 @stop
